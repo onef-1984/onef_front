3c696f7309822a1b69784e8357179cff0e7952f7
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-08-07 10:18:10 +0900

    feat : toast 라이브러리 변경

--- src/hooks/useMutation/useProfileMutation.ts
+++ src/hooks/useMutation/useProfileMutation.ts
@@ -1,6 +1,7 @@
 import { AuthMutation } from "@/apis/reactQuery/Mutation/AuthMutation";
 import { ImageMutation } from "@/apis/reactQuery/Mutation/ImageMutation";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 export const useProfileMutation = () => {
   const queryClient = useQueryClient();
@@ -8,16 +9,17 @@ export const useProfileMutation = () => {
   const authMutation = new AuthMutation();
 
   const { mutate } = useMutation({
-    mutationFn: async ({ formData, nickname }: { formData: FormData; nickname: string }) => {
+    mutationFn: async ({ formData, nickname, bio }: { formData: FormData; nickname: string; bio: string }) => {
       const { imageUrl } = await imageMutation.postImages("multi-upload")(formData);
 
-      await authMutation.patchProfile()({ profileImage: imageUrl[0], nickname });
+      await authMutation.patchProfile()({ profileImage: imageUrl[0], nickname, bio });
     },
     onSuccess: () => {
+      toast.success("프로필이 변경되었습니다.");
       queryClient.invalidateQueries({ queryKey: ["user"] });
     },
     onError: (error) => {
-      console.error(error);
+      toast.error(error.message);
     },
   });
 
