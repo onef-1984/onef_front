0574e48e77cc3286f43fef3a10c815f28ab02c50
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-08-08 17:57:12 +0900

    fix : 빌드 에러 해결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import LayoutWrapper from "@/components/layoutWrapper/LayoutWrapper";
 import { useRouterAdv } from "@/hooks/useRouterAdv";
 import Head from "next/head";
+import { useEffect } from "react";
 
 export default function Home() {
   const { push } = useRouterAdv();
-  push("/search");
+
+  useEffect(() => {
+    push("/search");
+  });
 
   return (
     <>
