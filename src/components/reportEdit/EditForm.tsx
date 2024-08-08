288d9f4053b784e6386120fa1e000b96fd9a854b
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-08-08 17:27:54 +0900

    feat : 제목 제한 추가

--- src/components/reportEdit/EditForm.tsx
+++ src/components/reportEdit/EditForm.tsx
@@ -25,6 +25,8 @@ export default function EditForm() {
         if (tagList.length > 10) return toast.error("태그는 최대 10개까지 입력 가능합니다.");
         if (data.content.length > 1984) return toast.error("내용은 1984자 이하로 입력해주세요.");
         if (data.content.length < 84) return toast.error("내용은 84자 이상 입력해주세요.");
+        if (data.title.length > 19) return toast.error("제목은 19자 이하로 입력해주세요.");
+        if (data.title.length < 4) return toast.error("제목은 4자 이상 입력해주세요.");
 
         mutate({ ...data, tags: tagList });
       })}
