971d13b1a3c2a006e735c227d77a4ac6c8fdaf40
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-11-27 14:52:45 +0900

    feat : invalidateQueries 추가

--- src/hooks/useMutation/useSocialLoginMutation.ts
+++ src/hooks/useMutation/useSocialLoginMutation.ts
@@ -1,11 +1,12 @@
 import fetcher from "@/apis/axios";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { Dispatch, SetStateAction } from "react";
 import { useRouterAdv } from "../useRouterAdv";
 
 export default function useSocialLoginMutation() {
   const { push } = useRouterAdv();
+  const queryClient = useQueryClient();
 
   const { mutate } = useMutation({
     mutationFn: ({ url }: { url: string; setErrorMessage: Dispatch<SetStateAction<string>> }) =>
@@ -14,6 +15,8 @@ export default function useSocialLoginMutation() {
         method: "post",
       }),
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"], refetchType: "all" });
+
       push("/");
     },
     onError: (error, { setErrorMessage }) => {
