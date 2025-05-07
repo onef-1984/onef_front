73dc94fc1c4f3b997dd6d89deb736540031a22dc
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-05-07 12:32:11 +0900

    refactor: useComment.query.ts에서 getComment 메서드를 화살표 함수에서 일반 메서드로 변경하고 thisBind 데코레이터 추가

--- src/apis/useDomain/useComment.query.ts
+++ src/apis/useDomain/useComment.query.ts
@@ -1,8 +1,13 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useQuery } from "@tanstack/react-query";
 import { CommentQuery } from "../Domains/Comment/Comment.query";
+import { thisBind } from "../Decorator/thisBind";
 
+@thisBind
 export class useCommentQuery {
   private commentQuery = new CommentQuery();
 
-  getComment = (reportId: string) => useQuery(this.commentQuery.getComments(reportId));
+  getComment(reportId: string) {
+    return useQuery(this.commentQuery.getComments(reportId));
+  }
 }
