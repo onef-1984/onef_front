ad9cc762ffe7f5cd54c1279ecdeb14ba5963f821
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-05-12 03:08:01 +0900

    feat: add Swiper for carousel functionality and remove Slick dependencies

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,8 +1,14 @@
-import Slider from "react-slick";
 import CardReport from "../card/CardReport";
-import styles from "./Carousel.module.css";
 import { useWindowSize } from "@/hooks/useWindowSize";
 import { GetReportListBySearchQuery } from "@/types/graphql.types";
+import { IoMdArrowDropleftCircle, IoMdArrowDroprightCircle } from "react-icons/io";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useRef } from "react";
+import styles from "./Carousel.module.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 type CarouselProps = {
   title: string;
@@ -15,21 +21,6 @@ export default function Carousel({ items, title }: CarouselProps) {
   if (windowWidth === 0) return <></>;
   else if (windowWidth > 768) return <BigCarousel title={title} items={items} />;
   else return <SmallCarousel title={title} items={items} />;
-}
-
-function BigCarousel({ items, title }: CarouselProps) {
-  return (
-    <div className={styles.root}>
-      <h2 className={styles.title}>{title}</h2>
-      <Slider {...settings}>
-        {items.map((item) => (
-          <div key={item.id} className={styles.card}>
-            <CardReport {...item} />
-          </div>
-        ))}
-      </Slider>
-    </div>
-  );
 }
 
 function SmallCarousel({ items, title }: CarouselProps) {
@@ -47,25 +38,58 @@ function SmallCarousel({ items, title }: CarouselProps) {
   );
 }
 
-const settings = {
-  speed: 700,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  infinite: true,
-  responsive: [
-    {
-      breakpoint: 1400,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      },
-    },
-    {
-      breakpoint: 1100,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-  ],
-};
+function BigCarousel({ items, title }: CarouselProps) {
+  const uniqueIdRef = useRef(`big-carousel-${Math.random().toString(36).slice(2, 9)}`);
+
+  const prevId = `${uniqueIdRef.current}-prev`;
+  const nextId = `${uniqueIdRef.current}-next`;
+
+  return (
+    <div className={styles.root}>
+      <h2 className={styles.title}>{title}</h2>
+
+      <div className={styles.sliderWrapper}>
+        <button className={`${styles.navButton} ${styles.prev}`} id={prevId}>
+          <IoMdArrowDropleftCircle />
+        </button>
+        <button className={`${styles.navButton} ${styles.next}`} id={nextId}>
+          <IoMdArrowDroprightCircle />
+        </button>
+
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={24}
+          loopAdditionalSlides={4}
+          slidesPerView={4}
+          slidesPerGroup={4}
+          navigation={{
+            nextEl: `#${nextId}`,
+            prevEl: `#${prevId}`,
+          }}
+          breakpoints={{
+            1100: {
+              slidesPerView: 4,
+              spaceBetween: 24,
+            },
+            900: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            600: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+          }}
+          loop
+          style={{ height: "100%", padding: "0 0 24px" }}
+        >
+          {items.map((item) => (
+            <SwiperSlide key={item.id}>
+              <CardReport {...item} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
