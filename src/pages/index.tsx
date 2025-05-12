791cf5189b7bfb18bed228edbe75e6b662560a77
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-05-12 16:04:27 +0900

    feat: 서버 사이드에서 보고서 데이터를 미리 가져오는 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
+import { ReportQuery } from "@/apis/Domains/Report/Report.query";
 import HeadMetaTag from "@/components/HeadMetaTag/HeadMetaTag";
 import HeroSec from "@/components/heroSec/HeroSec";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
+
+export async function getServerSideProps() {
+  const queryClient = new QueryClient();
+  const reportQuery = new ReportQuery();
+
+  await queryClient.prefetchQuery(reportQuery.getMostLikedReportList());
+  await queryClient.prefetchQuery(reportQuery.getRecentReportList());
+
+  return {
+    props: { dehydratedState: dehydrate(queryClient) },
+  };
+}
 
 export default function Home() {
   return (
